7aaba6c2e88cc2213f836d2c7b391d1138d8e75d
0-don <70039285+0-don@users.noreply.github.com>
2025-03-06 21:38:56 +0100

    asd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,39 @@
 module.exports = {
-  content: [
-    './src/**/*.html',
-    './**.html'
-  ],
-  darkMode: 'media',
+  content: ["./src/**/*.html", "./**.html", "./src/*/**.ts"],
+  darkMode: "media",
   theme: {
     gridTemplateAreas: {
-      'layout': [
-        'left middle middle middle right',
-      ],
+      layout: ["left middle middle middle right"],
     },
     extend: {
       fontFamily: {
         rajdhani: "'Rajdhani', sans-serif",
-        vt323: "'VT323', monospace"
+        vt323: "'VT323', monospace",
       },
       colors: {
-        '300': '#1e181e',
-        '500': '#191a1e',
-        'primary-200': '#f4908b',
-        'primary-300': '#ea6f69',
-        'primary-500': '#e8615a',
-        'primary-600': '#9c3230',
-        'primary-700': '#5e2122',
-        'primary-800': '#451717',
-        'primary-900': '#3c181a',
-        'secondary-500': '#2be4ea',
-        'secondary-900': '#295459',
-        'tertiary-500': '#fed33f',
-        'active-500': '#2bfea0',
+        300: "#1e181e",
+        500: "#191a1e",
+        "primary-200": "#f4908b",
+        "primary-300": "#ea6f69",
+        "primary-500": "#e8615a",
+        "primary-600": "#9c3230",
+        "primary-700": "#5e2122",
+        "primary-800": "#451717",
+        "primary-900": "#3c181a",
+        "secondary-500": "#2be4ea",
+        "secondary-900": "#295459",
+        "tertiary-500": "#fed33f",
+        "active-500": "#2bfea0",
       },
       backgroundImage: {
-        'elips': "radial-gradient(ellipse at 33% 10%, #461616 0%, transparent 75%)"
-      }
+        elips:
+          "radial-gradient(ellipse at 33% 10%, #461616 0%, transparent 75%)",
+      },
     },
   },
   plugins: [
-      require('@savvywombat/tailwindcss-grid-areas'),
-      require('@tailwindcss/forms'),
-      require('@tailwindcss/typography'),
-    ],
-
-}
+    require("@savvywombat/tailwindcss-grid-areas"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+  ],
+};
